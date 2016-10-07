679d8fd1561b73a1675c9cf93303dd099a31b41d
guhanda <guhanda@yahoo.com.br>
2016-10-07 17:45:16 -0300

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('Serviço funfando. agora é parar de preguiça e implementar essa merda');
 });
 
 module.exports = router;
